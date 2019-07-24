fb105356330545815d7d1a2f263a41586acb2664
xuhang <1564285867@qq.com>
2019-07-24 23:14:57 +0800

    【增加】 for循环代码

---
+++ 每日任务/while循环.py
@@ -0,0 +1,29 @@
+import os
+def showListData():
+    names = ['xuhang','wangshan','jiangfeng']
+    for name in names:
+        print(name)
+    print("for is down")
+
+#for 关键词: continue break
+#break  t退出这个循环
+#contie  继续执行循环，但是会掠过下面的语句
+def addNumber(x,y):
+    print("x= ",x," y= ",y)
+    addresoult = 0
+    for i in  range(0,x):
+        addresoult = addresoult + i
+        print(addresoult)
+    print("result = " , addresoult)
+
+
+def showContinue():
+    for i in range(0,3):
+        print("执行第：",i,"此")
+        if i == 1:
+            #continue
+            break
+        print("执行下面的语句：i = ",i)
+showContinue()
+#showListData()
+#addNumber(100,6)
