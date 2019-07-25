a71255bd38bb614fe14dcd691bb70582a1176f00
xuhang <1564285867@qq.com>
2019-07-25 21:44:39 +0800

    【增加】乘法口诀

---
+++ 每日任务/乘法口诀表.py
@@ -0,0 +1,10 @@
+import os
+def chengfa():
+    for i in range(1,10):
+        for j in range(1,i+1):
+            #print默认会换行，如果不希望换行则后面加上 end=''，表示以''结束而不是换行符
+            print(j,"*",i,"=",j*i,"  ",end='')
+        print("")
+            
+chengfa()      
+    
